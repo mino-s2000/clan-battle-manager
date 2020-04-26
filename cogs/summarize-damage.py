21978d610be6a230b9f6d9e9eebc8f7be41e8cfa
Mino <mino.s2000@outlook.com>
2020-04-26 10:50:03 +0900

    use google vision api

--- cogs/summarize-damage.py
+++ cogs/summarize-damage.py
@@ -1,67 +1,57 @@
-import configparser
 import discord
-import re
-import gspread
-import json
+import io
+import os
+import requests
 
 from datetime import datetime
-from discord.ext import commands, tasks
-from oauth2client.service_account import ServiceAccountCredentials
+from discord.ext import commands
+from google.cloud import vision
+from google.cloud.vision import types
+from google.oauth2 import service_account
 
-SECTION = 'dev'
-#SECTION = 'prod'
-
-class ConnectSpreadSheets:
-    def __init__(self, key, keyfile):
-        self.key = key
-        self.keyfile = keyfile
-
-    def connect_workbook(self):
-        scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(self.keyfile, scope)
-        gc = gspread.authorize(credentials)
-        return gc.open_by_key(self.key)
+IMG_CHANNEL_ID = 669477531061190666
 
 class SummarizeDamageCog(commands.Cog):
-    target_date = '2020-01-25'
-
     def __init__(self, bot):
         self.bot = bot
-        self.config_path = 'config.ini'
-        self.config = configparser.ConfigParser()
-        self.config.read(self.config_path, encoding='UTF-8')
-        self.config_section = self.config[SECTION]
-        self.spread_api_file = self.config_section.get('GSpreadApiKeyFile')
-        self.sheet_key = self.config_section.get('GSpreadSheetKey')
 
-    @commands.group(aliases=['d'])
-    async def damage(self, ctx):
-        if ctx.invoked_subcommand is None:
+    def download_img(self, uri, filename):
+        r = requests.get(uri, stream=True)
+        if r.status_code == 200:
+            with open(f'img/{filename}', 'wb') as f:
+                f.write(r.content)
+
+    def remove_img(self, filename):
+        os.remove(f'img/{filename}')
+
+    def detect_text(self, filename):
+        credentials = service_account.Credentials.from_service_account_file('discordbotprj-c32cb217f63c.json')
+        client = vision.ImageAnnotatorClient(credentials=credentials)
+
+        with io.open(f'img/{filename}', 'rb') as image_file:
+            content = image_file.read()
+
+        image = vision.types.Image(content=content)
+        response = client.text_detection(image=image)
+        texts = response.text_annotations
+        print('Texts:')
+        for text in texts:
+            print('\n"{}"'.format(text.description))
+        return texts[0].description
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.channel.id != IMG_CHANNEL_ID:
+            return
+        if message.attachments == []:
             return
 
-    @damage.command(aliases=['set', 's'])
-    async def setdate(self, ctx, date: str):
-        pattern = '^\d{4}-\d{2}-\d{2}$'
-        repatter = re.compile(pattern)
-        result = repatter.match(date)
-        if result:
-            self.target_date = date
-            await ctx.send(f'対象日付をセットしました。: {self.target_date}')
-        else:
-            await ctx.send('対象日付のフォーマットは、”yyyy-MM-dd" です。')
-
-    @damage.command(aliases=['a'])
-    async def add(self, ctx, user: str, target: str, damage: int, la = 'False'):
-        values = [self.target_date, user, target, damage, bool(la)]
-
-        workbook = ConnectSpreadSheets(self.sheet_key, self.spread_api_file).connect_workbook()
-        worksheet = workbook.sheet1
-        row_count = len(worksheet.get_all_values())
-        cell_list = worksheet.range(row_count + 1, 1, row_count + 1, 5)
-        for cell in cell_list:
-            cell.value = values[cell_list.index(cell)]
-        worksheet.update_cells(cell_list)
-        await ctx.send('データを追加しました。')
+        for img in message.attachments:
+            self.download_img(img.url, img.filename)
+            text = self.detect_text(img.filename)
+            await message.channel.send(content=text)
+            await message.add_reaction('\U0001F44D')
+            self.remove_img(img.filename)
 
 def setup(bot):
     bot.add_cog(SummarizeDamageCog(bot))
